1d1ed077b96d0023c6c97d11909993ee3fd8cdcc
szymon <wolnyszym@gmail.com>
2019-08-20 18:20:38 +0200

    refactoring

---
+++ common.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+
+TODAY_DATE = datetime.today().strftime('%Y-%m-%d')
+BASE_CURRENCIES = ['CAD', 'HKD', 'ISK', 'PHP', 'DKK', 'HUF', 'CZK', 'GBP', 'RON', 'SEK', 'IDR', 'INR', 'BRL', 'RUB',
+                   'HRK', 'JPY', 'THB', 'CHF', 'EUR', 'MYR', 'BGN', 'TRY', 'CNY', 'NOK', 'NZD', 'ZAR', 'USD', 'MXN',
+                   'SGD', 'AUD', 'ILS', 'KRW', 'PLN']
+COLORS = {
+    'background': '#111111',
+    'text': '#7FDBFF'
+}
